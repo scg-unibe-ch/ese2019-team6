27e862bbf172c9e722645cb46aea141bb68cdaa5
gllamphar <gllamphar@users.noreply.github.com>
2019-11-06 01:09:42 +0100

    Toast on Recovery Password page working

--- frontend/src/app/newpass/newpass.page.ts
+++ frontend/src/app/newpass/newpass.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-newpass',
   templateUrl: './newpass.page.html',
   styleUrls: ['./newpass.page.scss'],
 })
-export class NewpassPage implements OnInit {
+export class NewpassPage {
+  email = '';
+  password = '';
+  error = '';
+  image: number;
+  constructor(
+    private fireauth: AngularFireAuth,
+    private router: Router,
+    private toastController: ToastController,
+    ) {
 
-  constructor() { }
+  }
 
-  ngOnInit() {
+  recover() {
+    this.fireauth.auth.sendPasswordResetEmail(this.email)
+      .then(data => {
+        console.log(data);
+        this.presentToast('Password reset email sent', true, 'bottom', 3000);
+        this.router.navigateByUrl('/login');
+      })
+      .catch(err => {
+        console.log(` failed ${err}`);
+        this.error = err.message;
+      });
+  }
+
+  async presentToast(message, showButton, position, duration) {
+    const toast = await this.toastController.create({
+      message,
+      showCloseButton: showButton,
+      position,
+      duration
+    });
+    await toast.present();
   }
 
 }
